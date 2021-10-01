644640028bf415b76ad56b6b55ef12392fa48e98
estebanRojasOjeda <esteban1987@protonmail.com>
2021-09-30 23:57:08 -0300

    finish

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const Pokemon = (props) => {
 
     const getPokemons = () => {
-        axios.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=807&offset=200')
             .then(res => {
                 console.log(res.data.results)
                 props.setPokemon(res.data.results)
